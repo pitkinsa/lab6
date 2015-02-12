2679e4b4937de3eb80fe6f5130a8be125aef64ba
Sachi Pitkin <spitkin@ucsd.edu>
2015-02-12 22:01:39 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,6 +12,7 @@ function initializePage() {
 	$('.project a').click(addProjectDetails);
 
 	$('#colorBtn').click(randomizeColors);
+
 }
 
 /*
@@ -25,8 +26,22 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
 	console.log("User clicked on project " + idNumber);
+	var url = "/project/" + idNumber;
+	console.log("Requested url is "+url);
+	$.get(url, emptyCallback);
+}
+function emptyCallback(result){
+	console.log(result);
+	var projectHTML = '<a href="#" class="thumbnail">' + 
+	    '<img src="' + result['image'] + '"class="detailsImage">' +
+	    '<p>' + result['title'] + '</p>' +
+	    '<p><small>' + result['date'] + '</small></p>' +
+	    '<p>' + result['summary'] + '</p></a>';
+
+	$("#project"+result['id']).find(".details").html(projectHTML);
+
+	console.log("done");
 }
 
 /*
@@ -35,4 +50,22 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	var url = "/palette";
+	$.get(url, colorCallback);
 }
+function colorCallback(result){
+	console.log(result);
+	var obj = result['colors'];
+	var colors = obj['hex'];
+	console.log(colors);
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+
+
+
